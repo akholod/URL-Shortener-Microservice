1aa06710df0b9ff9bcd4f2df2a4b668528240792
Andrey <andrey.kokhanevich@gmail.com>
2016-10-13 21:12:34 +0000

    add routes

--- routes/index.js
+++ routes/index.js
@@ -5,12 +5,18 @@ const dbModel = require("../schemas/url-short.js")
 module.exports = function(app) {
     
     app.get('/', (req, res) => {
-    var root = path.parse(__dirname)
-    res.sendFile(root.dir + '/public/home.html');
+        var root = path.parse(__dirname)
+        res.sendFile(root.dir + '/public/home.html');
     })
     
     app.get('/new/:url(*)', (req, res) => {
+        var fullUrl = req.protocol + '://' + req.get('host') + '/';
         var par = (req.param('url'));
+        
+        if(par.search(/^(https?:\/\/)?([\w\.]+)\.([a-z]{2,6}\.?)(\/[\w\.]*)*\/?/) == -1) {
+            res.end(JSON.stringify({"Error":"Incorect url format or adress, please pass corect url"}));
+            return console.log('error');
+        }
         
        dbModel.findOne({"url": par}, (err, url) => {
             if (err){
@@ -19,7 +25,7 @@ module.exports = function(app) {
             if (url) {
                 res.send(JSON.stringify({
                     'original_url' : url.url,
-                    'short_url' : url.urlId
+                    'short_url' : fullUrl + url.urlId
                 }))
                 return
             } 
@@ -33,10 +39,28 @@ module.exports = function(app) {
                 }
                 res.send(JSON.stringify({
                     'original_url' : record.url,
-                    'short_url' : record.urlId
+                    'short_url' : fullUrl + record.urlId
                 }))
             })
             
+        })
+    })
+    
+    app.get('/:shortUrl(*)', (req, res) => {
+        var par = (req.param('shortUrl'));
+        if(isNaN(par)) {
+            res.end(JSON.stringify({"Error":"Incorect short url"}));
+            return console.error(par + ' is NaN');
+        }
+        dbModel.findOne({'urlId' : par} , (err, url) => {
+            if (err){
+               return console.error(err);
+            } 
+            if (url) {
+                res.redirect(url.url)
+            } else {
+                res.end(JSON.stringify({"Error":"No such short url in database, please pass existing short url"}));
+            }
         })
     })
 }
